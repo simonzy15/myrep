0b754a82debf4c09e71a055f935ae8bbca2b9e5f
Simon Yan <zj2yan@uwaterloo.ca>
2021-05-29 13:27:04 -0400

    simple post api

--- myrep-app/src/app/app.module.ts
+++ myrep-app/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 
 import { ShowHidePasswordModule } from 'ngx-show-hide-password';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import { ShowHidePasswordModule } from 'ngx-show-hide-password';
     BrowserModule,
     ShowHidePasswordModule,
     AppRoutingModule,
+    HttpClientModule,
     
     FormsModule,
     ReactiveFormsModule,
--- myrep-app/src/app/login/login.component.ts
+++ myrep-app/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -8,11 +9,13 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
+  public path: string;
   
   constructor(
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private http: HttpClient
   ) {
-
+    this.path = 'placeholder';
   }
 
   ngOnInit(): void {
@@ -27,5 +30,9 @@ export class LoginComponent implements OnInit {
   }
   onSubmit(): void{
     console.log(this.loginForm);
+    console.log(this.loginForm.value);
+    this.http.post(this.path, this.loginForm.value).subscribe((result) =>{
+        console.warn("Result: ", result)
+    })
   }
 }
