0346ff80a929a588a6156adce167c8d3abbbc44e
Simon Yan <zj2yan@uwaterloo.ca>
2021-06-05 13:57:04 -0400

    basic router

---
+++ backend/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type Register struct {
+	User  *User  `jason:"user"`
+	Email string `json:"email"`
+}
+
+type User struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+var users []User
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var user User
+	_ = json.NewDecoder(r.Body).Decode(&user)
+	user.ID = strconv.Itoa(rand.Intn(100000000))
+	users = append(users, user)
+	json.NewEncoder(w).Encode(user)
+}
+
+func getUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	json.NewEncoder(w).Encode(users)
+	for _, item := range users {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode("no item found")
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/api/register", createUser).Methods("POST")
+	router.HandleFunc("/api/getuser/{id}", getUser).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8001", router))
+}
