b6c7b7dd9ecdd93f4696b50c497d6007b29ef10f
Simon Yan <zj2yan@uwaterloo.ca>
2021-06-18 13:22:57 -0400

    working with auth0

--- myrep-app/src/app/app-routing.module.ts
+++ myrep-app/src/app/app-routing.module.ts
@@ -1,13 +1,9 @@
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home';
-import { LoginComponent } from './login';
-import { SignupComponent } from './signup';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'login', component: LoginComponent },
-    { path: 'signup', component: SignupComponent },
 
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
--- myrep-app/src/app/login/login.component.ts
+++ myrep-app/src/app/login/login.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-login',
@@ -8,31 +7,16 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  loginForm: FormGroup;
-  public path: string;
   
   constructor(
-    private fb: FormBuilder,
-    private http: HttpClient
+    public auth: AuthService,
   ) {
-    this.path = 'placeholder';
   }
 
   ngOnInit(): void {
-    this.initForm();
   }
 
-  private initForm(): void {
-    this.loginForm = this.fb.group({
-      Username: '',
-      Password: ''
-    })
-  }
-  onSubmit(): void{
-    console.log(this.loginForm);
-    console.log(this.loginForm.value);
-    this.http.post(this.path, this.loginForm.value).subscribe((result) =>{
-        console.warn("Result: ", result)
-    })
+  public loginWithRedirect(): void {
+    this.auth.loginWithRedirect();
   }
 }
