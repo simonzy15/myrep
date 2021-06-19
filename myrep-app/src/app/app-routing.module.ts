d213627c31eda281cf38870cee4d3ed2025e04b1
Simon Yan <zj2yan@uwaterloo.ca>
2021-06-19 13:28:49 -0400

    profile creation

--- myrep-app/src/app/app-routing.module.ts
+++ myrep-app/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home';
+import { ProfileComponent } from './profile';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
+    { path: 'profile', component: ProfileComponent },
 
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
