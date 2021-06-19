31e60dc9969ade0d6f1dad8adc342c06b4d57d8b
Simon Yan <zj2yan@uwaterloo.ca>
2021-06-19 13:28:49 -0400

    profile creation

---
+++ myrep-app/src/app/profile/profile.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+
+  constructor(
+    public auth: AuthService
+  ) { }
+
+  ngOnInit(): void {
+  }
+}
