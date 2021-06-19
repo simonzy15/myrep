3dd390dfffcd9b7af6391f6125eec5cea363140e
M <max.zhu.sss@gmail.com>
2021-06-19 15:36:01 -0400

    wtf

--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
+	"fmt"
 	"log"
 	"math/rand"
+	"myrep/pkg1"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/gorilla/mux"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type Register struct {
@@ -22,6 +28,8 @@ type User struct {
 }
 
 var users []User
+var DB *sql.DB
+var currentUser User
 
 func createUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
@@ -41,19 +49,87 @@ func getUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 	json.NewEncoder(w).Encode(users)
-	for _, item := range users {
-		if item.ID == params["id"] {
-			json.NewEncoder(w).Encode(item)
-			return
-		}
-	}
-	json.NewEncoder(w).Encode(users)
+
+	fmt.Println(params["id"])
+
+	// userinfo, err := db.Query("select USER_ID, USER_NAME from USERS where USER_ID = 12")
+	// if err != nil {
+	// 	// return
+	// 	log.Fatal(err)
+	// 	return
+	// }
+
+	// defer userinfo.Close()
+
+	// var (
+	// 	id   int
+	// 	name string
+	// )
+
+	// for userinfo.Next() {
+	// 	err := userinfo.Scan(&id, &name)
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	log.Println(id, name)
+	// }
+
+	// for _, item := range users {
+	// 	if item.ID == params["id"] {
+	// 		json.NewEncoder(w).Encode(item)
+	// 		return
+	// 	}
+	// }
+	// json.NewEncoder(w).Encode(users)
 }
 
 func main() {
+
+	// database connection
+
+	DB, err := sql.Open("mysql", "admin:123234w1nd0w@tcp(myrep-db.cqmo4rbwc6vu.us-east-2.rds.amazonaws.com:3306)/USER_DATA")
+	if err != nil {
+		panic(err)
+	}
+
+	err = DB.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	userinfo, err := DB.Query("select USER_ID, USER_NAME from USERS where USER_ID = 12")
+	if err != nil {
+		// return
+		log.Fatal(err)
+		return
+	}
+
+	defer userinfo.Close()
+
+	var (
+		id   int
+		name string
+	)
+
+	for userinfo.Next() {
+		err := userinfo.Scan(&id, &name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(id, name)
+	}
+	// See "Important settings" section.
+	DB.SetConnMaxLifetime(time.Minute * 3)
+	DB.SetMaxOpenConns(10)
+	DB.SetMaxIdleConns(10)
+
 	router := mux.NewRouter()
 	router.HandleFunc("/api/register", createUser).Methods("POST")
 	router.HandleFunc("/api/getuser/{id}", getUser).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8001", router))
+
+	pkg1.Add()
+
+	// defer db.Close()
 }
---
+++ backend/pkg1/test.go
@@ -0,0 +1,7 @@
+package pkg1
+
+import "fmt"
+
+func Add() {
+    fmt.Println("Hello")
+}
