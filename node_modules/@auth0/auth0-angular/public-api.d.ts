66dfefc45661ed9581df4cef46fdd13ec90e3c7b
M <max.zhu.sss@gmail.com>
2021-06-20 12:11:06 -0400

    removed node modules oopsie

--- node_modules/@auth0/auth0-angular/public-api.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './lib/auth.service';
-export * from './lib/auth.module';
-export * from './lib/auth.guard';
-export * from './lib/auth.interceptor';
-export * from './lib/auth.config';
