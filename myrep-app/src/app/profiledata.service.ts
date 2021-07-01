6da7e42cf12e9aa64462f02825f454df190af098
Simon Yan <zj2yan@uwaterloo.ca>
2021-07-01 16:17:19 -0400

    fixed createuser

--- myrep-app/src/app/profiledata.service.ts
+++ myrep-app/src/app/profiledata.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@angular/core';
 import { environment as env } from '../environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { ProfileData } from './profile/profile.component';
+
+export interface ProfileData {
+  id: string;
+  username: string;
+  bio: string;
+  upvotes: string;
+  downvotes: string;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -9,23 +16,45 @@ import { ProfileData } from './profile/profile.component';
 export class ProfiledataService {
   public state: string;
   public path: string;
+  public profileData: ProfileData;
   constructor(
     private http: HttpClient
   ) {
     this.path = env.backendPath
   }
   
-  public getProfileData(): ProfileData {
-    this.http.get(
-      this.path + '/api/getuser/12',
+  public getProfileData(): void {
+    this.http.get<ProfileData>(
+      this.path + '/api/getuser/testaccount',
+    ).subscribe(
+      res => {
+        console.log(res)
+        if (res === null) {
+          this.createProfile(localStorage.getItem('username'))
+        }
+        else {
+          this.profileData = res
+        }
+      }
+    )
+  }
+  
+  public createProfile(username: any): void{
+    if (username === null) {
+      return
+    }
+    var body = JSON.stringify({
+      'username': username
+    })
+    console.log(body)
+    this.http.post<any>(
+      this.path + '/api/register',
+      body
     ).subscribe(
       res => {
         console.log(res)
       }
     )
-    return {
-      username: '',
-      bio: ''
-    }
   }
+
 }
