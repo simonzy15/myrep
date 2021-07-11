63c71e39d826757c5e8a2dceca6f611888cf7397
Simon Yan <zj2yan@uwaterloo.ca>
2021-07-11 14:05:05 -0400

    addcomment api call

--- myrep-app/src/app/user/user.component.ts
+++ myrep-app/src/app/user/user.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProfileData, ProfiledataService } from '../profiledata.service';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { environment as env } from '../../environments/environment';
 
 @Component({
   selector: 'app-user',
@@ -13,12 +15,16 @@ export class UserComponent implements OnInit {
   public targetUser: string;
   public profileData: ProfileData;
   public exists: boolean;
+  public path: string;
 
   constructor(
     private fb: FormBuilder,
+    private http: HttpClient,
     private router: Router,
     public profileDataService: ProfiledataService
-  ) { }
+  ) {
+    this.path = env.backendPath
+  }
 
   ngOnInit(): void {
     this.targetUser = this.router.url.split('/')[2];
@@ -41,9 +47,23 @@ export class UserComponent implements OnInit {
       comment: ''
     })
   }
+
+  public addComment(comment: string): void {
+    var body = JSON.stringify({
+      'username': this.targetUser, // user page
+      'commenter': localStorage.getItem('username'), // commenter
+      'comment': comment
+    })
+    this.http.post<any>(
+      this.path + '/api/addcomment',
+      body
+    ).subscribe()
+  }
+
   onSubmit(): void {
     const newComment = this.commentForm.value['comment']
     console.log(newComment)
+    this.addComment(newComment)
     this.commentForm.reset()
   }
 }
