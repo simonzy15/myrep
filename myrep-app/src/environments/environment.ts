ab8b042f861e5c16119d5640c5660bffc756ba1e
M <max.zhu.sss@gmail.com>
2021-07-31 21:28:50 -0400

    added environment.ts to gitignore

--- myrep-app/src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  auth: {
-    domain : 'dev-8z3b7ajm.us.auth0.com',
-    clientId : 'zwhMLIbxzfTWildS0YG0iTGQXXTYEJxj'
-  },
-  backendPath: 'https://myr3papi.link'
-};
